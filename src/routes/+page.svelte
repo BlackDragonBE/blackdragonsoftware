bd9f1f3ba73027d38e0760eb3c3da32125ee0f24
Eric Van de Kerckhove <blackdragon.be@gmail.com>
2023-02-02 19:27:10 +0100

    Add new logo design

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,60 +14,60 @@
 
 <div class="columns p-4">
 	<div class="column is-offset-1">
-		<div class="card scalehover" style="height: 100%">
-			<div class="card-image">
-				<figure class="image is-4by3">
-					<a href="https://blackdragonbe.itch.io/"><img src={itch} alt="itch.io" /></a>
-				</figure>
-			</div>
-			<div class="card-content">
-				<div class="media">
-					<div class="media-left">
-						<figure class="image is-48x48">
-							<i class="fab fa-itch-io fa-3x" />
-						</figure>
+		<a href="https://blackdragonbe.itch.io/">
+			<div class="card scalehover" style="height: 100%">
+				<div class="card-image">
+					<figure class="image is-4by3">
+						<img src={itch} alt="itch.io" />
+					</figure>
+				</div>
+				<div class="card-content">
+					<div class="media">
+						<div class="media-left">
+							<figure class="image is-48x48">
+								<i class="fab fa-itch-io fa-3x" />
+							</figure>
+						</div>
+						<div class="media-content">
+							<p class="title is-4">itch.io</p>
+							<p class="subtitle is-6">Game developer</p>
+						</div>
 					</div>
-					<div class="media-content">
-						<p class="title is-4"><a href="https://blackdragonbe.itch.io/">itch.io</a></p>
-						<p class="subtitle is-6">Game developer</p>
+					<div class="content">
+						I host most of my recent Unity games and old prototypes on itch.io. I'm a fan of game jams as well!
+						<br />
 					</div>
 				</div>
-
-				<div class="content">
-					I host most of my recent Unity games and old prototypes on itch.io. I'm a fan of game jams as well!
-					<br />
-				</div>
 			</div>
-		</div>
+		</a>
 	</div>
 	<div class="column">
-		<div class="card scalehover" style="height: 100%">
-			<div class="card-image">
-				<figure class="image is-4by3">
-					<a href="https://www.raywenderlich.com/gametech"><img src={rw} alt="Kodeco" /></a>
-				</figure>
-			</div>
-			<div class="card-content">
-				<div class="media">
-					<div class="media-left">
-						<figure class="image is-48x48">
-							<i class="fas fa-book fa-3x" />
-						</figure>
+		<a href="https://www.raywenderlich.com/gametech">
+			<div class="card scalehover" style="height: 100%">
+				<div class="card-image">
+					<figure class="image is-4by3">
+						<img src={rw} alt="Kodeco" />
+					</figure>
+				</div>
+				<div class="card-content">
+					<div class="media">
+						<div class="media-left">
+							<figure class="image is-48x48">
+								<i class="fas fa-book fa-3x" />
+							</figure>
+						</div>
+						<div class="media-content">
+							<p class="title is-4">Kodeco</p>
+							<p class="subtitle is-6">Team lead</p>
+						</div>
 					</div>
-					<div class="media-content">
-						<p class="title is-4">
-							<a href="https://www.kodeco.com/gametech">Kodeco</a>
-						</p>
-						<p class="subtitle is-6">Team lead</p>
+					<div class="content">
+						I'm the lead of an amazing group of people that teach people how to make games and experiences.
+						<br />
 					</div>
 				</div>
-
-				<div class="content">
-					I'm the lead of an amazing group of people that teach people how to make games and experiences.
-					<br />
-				</div>
 			</div>
-		</div>
+		</a>
 	</div>
 	<div class="column">
 		<div class="card scalehover" style="height: 100%">
