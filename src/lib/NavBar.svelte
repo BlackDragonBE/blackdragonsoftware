81522f277a3aff0f283839325e93396e7750a4c4
Eric Van de Kerckhove <blackdragon.be@gmail.com>
2023-02-02 13:24:42 +0100

    ok

--- src/lib/NavBar.svelte
+++ src/lib/NavBar.svelte
@@ -2,6 +2,7 @@
 	import { page } from '$app/stores';
 	import logo from '$lib/images/logo.svg';
 	import NavBarItem from './NavBarItem.svelte';
+	import logo_full from '$lib/images/logo-full.png';
 
 	let navbarActive: boolean = false;
 	let navbarClass: string;
@@ -9,10 +10,9 @@
 
 	$: navbarClass = navbarActive ? 'is-active' : '';
 	$: path = $page.url.pathname;
-
 </script>
 
-<nav class="navbar" aria-label="main navigation">
+<nav class="navbar is-primary" aria-label="main navigation">
 	<div class="navbar-brand">
 		<a class="navbar-item" href="/">
 			<img src={logo} width="40" height="28" />
@@ -37,13 +37,12 @@
 
 	<div id="navbarBasicExample" class="navbar-menu {navbarClass}">
 		<div class="navbar-start">
-			<NavBarItem path={path} link='/' linkName='Home' faIcon='fas fa-home'/>
-			<NavBarItem path={path} link='/Games' linkName='Games' faIcon='fas fa-gamepad'/>
-			<NavBarItem path={path} link='/3D' linkName='3D' faIcon='fas fa-cube'/>
-			<!-- <NavBarItem path={path} link='/unity' linkName='Unity' faIcon='fab fa-unity'/>
-			<NavBarItem path={path} link='/godot' linkName='Godot' faIcon='fas fa-home'/> -->
-			<!-- <a class="navbar-item"> Documentation </a>
-			<div class="navbar-item has-dropdown is-hoverable">
+			<NavBarItem {path} link="/" linkName="Home" faIcon="fas fa-home" />
+			<NavBarItem {path} link="/Games" linkName="Games" faIcon="fas fa-gamepad" />
+			<NavBarItem {path} link="/3D" linkName="3D Art" faIcon="fas fa-cube" />
+			<NavBarItem {path} link="/About" linkName="About" faIcon="fas fa-cube" />
+
+			<!-- <div class="navbar-item has-dropdown is-hoverable">
 				<a class="navbar-link"> More </a>
 
 				<div class="navbar-dropdown">
@@ -54,6 +53,21 @@
 					<a class="navbar-item"> Report an issue </a>
 				</div>
 			</div> -->
+		</div>
+
+		<div class="navbar-end">
+			<div class="navbar-item">
+				<div class="field is-grouped">
+					<p class="control">
+						<a class="button is-primary" href="https://twitter.com/BlackDragonBE">
+							<span class="icon">
+								<i class="fab fa-twitter" aria-hidden="true" />
+							</span>
+							<span>@BlackDragonBE</span>
+						</a>
+					</p>
+				</div>
+			</div>
 		</div>
 	</div>
 </nav>
---
+++ src/routes/About/+page.svelte
@@ -0,0 +1,8 @@
+<section class="section">
+	Black Dragon Software is consists of single (game) developer. Some of my hobbies include developing games, scripting and creating &amp;
+	printing 3D models.<br />
+	My weapons of choice are <a href="https://unity.com/">Unity</a>,
+	<a href="https://godotengine.org/">Godot</a>, <a href="https://www.blender.org/">Blender</a>,
+	<a href="https://code.visualstudio.com/">VS Code</a>
+	and <a href="https://www.raise3d.com/ideamaker/">Ideamaker</a>.
+</section>
